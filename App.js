0caecffd6804beed87e924469afb960c49333183
E. Huntley <ehuntley83@yahoo.com>
2018-10-02 16:22:20 -0400

    Adding a text input box

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 import Bananas from './components/Bananas';
 import BlinkingTextLabel from './components/BlinkingTextLabel';
 import TextLabel from './components/TextLabel';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <TextInput style={{height: 85}} 
+          placeholder="Type some text here!"
+          onChangeText={(text) => this.setState({text})} />
         <View style={styles.title}>
-          <TextLabel textStyle={styles.titleText} textString='-=< React Native tests >=-' />
+          <TextLabel textStyle={styles.titleText} textString={this.state.text} />
         </View>
         <View style={styles.content}>
           <BlinkingTextLabel text='Bananas!!!' timeIntervalMs={1500} />
